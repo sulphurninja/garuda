aba74e699e01defa5b1b409da55cda97ca0c5889
Aditya Pingle <95356721+sulphurninja@users.noreply.github.com>
2024-08-09 17:55:51 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDb = async () => {
   try {
     // Add your MongoDB connection URI here
-    const MONGODB_URI = 'mongodb+srv://aditya4sure:garuda@garuda.8wznpts.mongodb.net/?retryWrites=true&w=majority&appName=Garuda';
+    const MONGODB_URI = 'mongodb+srv://aditya4sure:aditya@cluster0.efo7n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
     await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
